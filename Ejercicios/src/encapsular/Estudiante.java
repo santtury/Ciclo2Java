2241cfa8ef7b05d011ee68bd884492e693f55d43
Santtury <antturysebastian@gmail.com>
2021-07-31 23:18:27 -0500

    Se realizó el ejercicio de encapsular

--- Ejercicios/src/encapsular/Estudiante.java
+++ Ejercicios/src/encapsular/Estudiante.java
@@ -3,11 +3,45 @@ package encapsular;
 public class Estudiante {
     // Atributos
     private String nombre;
-    private int nota1;
-    private int nota2;
-    private int nota3;
+    private double nota1;
+    private double nota2;
+    private double nota3;
 
     // Métodos
+    public double promedio(double nota1, double nota2, double nota3) {
+        return (nota1 + nota2 + nota3) / 3;
+    }
 
-    // Sets y Gets
+    // Gets y Sets
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getNota1() {
+        return nota1;
+    }
+
+    public void setNota1(double nota1) {
+        this.nota1 = nota1;
+    }
+
+    public double getNota2() {
+        return nota2;
+    }
+
+    public void setNota2(double nota2) {
+        this.nota2 = nota2;
+    }
+
+    public double getNota3() {
+        return nota3;
+    }
+
+    public void setNota3(double nota3) {
+        this.nota3 = nota3;
+    }
 }
--- Ejercicios/src/encapsular/Main.java
+++ Ejercicios/src/encapsular/Main.java
@@ -1,7 +1,26 @@
 package encapsular;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Estudiante e1 = new Estudiante();
 
+        System.out.println("Nombre del estudiante");
+        e1.setNombre(sc.nextLine());
+
+        System.out.println("Digite la nota 1");
+        e1.setNota1(Double.parseDouble(sc.nextLine()));
+
+        System.out.println("Digite la nota 2");
+        e1.setNota2(Double.parseDouble(sc.nextLine()));
+
+        System.out.println("Digite la nota 3");
+        e1.setNota3(Double.parseDouble(sc.nextLine()));
+
+        System.out.println("Nombre del estudiante: " + e1.getNombre());
+        System.out.println("Promedio del estudiante: " + e1.promedio(e1.getNota1(), e1.getNota2(), e1.getNota3()));
+        sc.close();
     }
 }
